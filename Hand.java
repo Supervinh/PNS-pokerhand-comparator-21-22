5167c3f4875c23d3570b6edd09526fa1d13815a9
Matis Herrmann <matis.herrmann@etu.unice.fr>
2021-09-29 16:07:10 +0200

    Correction de faute d'ortographe

--- Hand.java
+++ Hand.java
@@ -11,7 +11,7 @@ public class Hand {
         this.playerName += Hand.playerNumber;
     }
 
-    @Override
+    @Override   
     public String toString() {
         return this.playerName + " " + card;
     }
