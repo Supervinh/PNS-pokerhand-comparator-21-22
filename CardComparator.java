50083dd63d4c4dd406bcb04cb9cf10c614b8752e
Matis Herrmann <matis_herrmann@hotmail.fr>
2021-10-06 17:50:43 +0200

    Final Init

--- CardComparator.java
+++
@@ -1,9 +0,0 @@
-import java.util.Comparator;
-
-public class CardComparator implements Comparator<Card> {
-    
-    @Override
-    public int compare(Card c1, Card c2) {
-        return c1.compareTo(c2);
-    }
-}
--- ResultPrinter.java
+++
@@ -1,9 +0,0 @@
-public class ResultPrinter {
-    public static void showResult (Hand h){
-        System.out.println("The Winner is " + h);
-    }
-
-    public static String getResult(Hand h) {
-        return "The Winner is " + h;
-    }
-}
