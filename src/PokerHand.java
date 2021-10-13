cf6d975f2d72b95daf960291bd8ac22bdbc11e88
Matis Herrmann <matis_herrmann@hotmail.fr>
2021-10-13 19:40:22 +0200

    Cleaning

--- src/PokerHand.java
+++ src/PokerHand.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 public class PokerHand {
     public static void main(String [] args) {
-        // Game Global Settings like Number of players, Cards and auto is in the Game Class as static fields.
+        // Game Global Settings like player number, card number and auto draw card is in the Game Class as static fields.
         ArrayList<Hand> players = new ArrayList<>();
         Card card;
 
