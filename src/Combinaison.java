9f79a1fa72a29c81c03702770c02585713a40f2a
Matis Herrmann <matis_herrmann@hotmail.fr>
2021-10-13 17:04:19 +0200

    Added Combainaison field to Hand and fixed ConsecutiveCards

--- src/Combinaison.java
+++ src/Combinaison.java
@@ -42,18 +42,13 @@ public class Combinaison {
         }
     }
 
-
     private int consecutiveCards(){
-        int compteur_max = 0;
-        int compteur = 0;
-        for (int i = 0; i < this.Rank_table.size(); i++){
-            if (this.Rank_table.get(i) != 0){
+        int compteur_max = 0, compteur = 0;
+        for (Integer integer : this.Rank_table) {
+            if (integer != 0) {
                 compteur++;
-            }
-            else{
-                if (compteur_max < compteur) {
-                    compteur_max = compteur;
-                }
+            } else {
+                compteur_max = Math.max(compteur, compteur_max);
                 compteur = 0;
             }
         }
--- src/Hand.java
+++ src/Hand.java
@@ -4,15 +4,17 @@ public class Hand {
     private static int playerNum = 0;
     private final ArrayList<Card> cards;
     private String name = "Player ";
-    private int score;
-    private int subscore;
+    private Combinaison combinaison;
+    private int score=0;
+    private int subscore=0;
 
     public Hand(ArrayList<Card> cards){
         Hand.playerNum++;
         this.cards = cards;
         this.name += Hand.playerNum;
         this.sortHand(this.cards);
-        this.evaluateHand();
+        this.combinaison = new Combinaison(this);
+
     }
 
     public void sortHand(ArrayList<Card> cards) {
