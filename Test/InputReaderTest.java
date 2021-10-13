71e537fe32b1c8b6363581bf7b032ec2f78f7a9d
unknown <narutovinh@hotmail.fr>
2021-10-13 17:43:40 +0200

    Avancement sur InputReaderTest

--- Test/InputReaderTest.java
+++ Test/InputReaderTest.java
@@ -3,16 +3,24 @@ import org.junit.jupiter.api.*;
 import java.io.*;
 
 class InputReaderTest {
-
     private Rank output = Rank.Sept;
-
+    private Suit output2 = Suit.Carreau;
     @Test
-    void readerTest(){
+    void rankTest(){
         final String test = "Sept";
 
         Rank rank = InputReader.typingRank(test);
 
         Assertions.assertEquals(rank, output);
+    }
+
+    @Test
+    void suitTest(){
+        final String test = "Carreau";
+
+        Suit suit = InputReader.typingSuit(test);
+
+        Assertions.assertEquals(suit, output2);
 
     }
 }
