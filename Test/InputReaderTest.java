b5be468781f47d9d6c84ddd4aacd1fae0a2fc072
unknown <narutovinh@hotmail.fr>
2021-10-20 00:16:54 +0200

    Problème ImputReaderTest

--- Test/InputReaderTest.java
+++ Test/InputReaderTest.java
@@ -1,27 +1,29 @@
 import org.junit.jupiter.api.*;
 
-import java.io.*;
 
 class InputReaderTest {
     private Rank output = Rank.Sept;
     private Suit output2 = Suit.Carreau;
     @Test
     void rankTest(){
-        final String test = "Sept";
-
+        final String test = "7";
         Rank rank = InputReader.typingRank(test);
-
         Assertions.assertEquals(rank, output);
     }
+
+    /*@Test
+    void rankTest2(){
+        final String test = "Cinquante";
+        Rank rank = InputReader.typingRank(test);
+        Assertions.assertEquals(rank, java.lang.IllegalArgumentException);
+
+    }*/
 
     @Test
     void suitTest(){
         final String test = "Carreau";
-
         Suit suit = InputReader.typingSuit(test);
-
         Assertions.assertEquals(suit, output2);
-
     }
 }
 
