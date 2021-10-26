2113f9ecc27e47f7af8fa424d9d35f35fa3eb35f
Matis Herrmann <matis_herrmann@hotmail.fr>
2021-10-26 11:18:33 +0200

    Modified the JavaDoc

--- src/Deck.java
+++ src/Deck.java
@@ -9,16 +9,20 @@ import java.util.Random;
  * @author Vinh Faucher
  */
 public class Deck {
+    // Variables qui Stock les cartes du packet.
     private ArrayList<Card> deck = new ArrayList<>();
 
+    /**
+     * Constructeur de la classe, et initialise le packet de carte.
+     */
     public Deck() {
-        // Pour chaque rang on ajoute dans le deck tous les symboles de ce rang ie (pour 2 -> 2Co, 2Pi, 2Ca, 2Tr)
+        // Pour chaque rang on ajoute dans le deck tous les symboles de ce rang ie (pour 2 -> 2Co, 2Pi, 2Ca, 2Tr).
         for (Rank rank : Rank.values()) {
             for (Suit suit : Suit.values()) {
                 deck.add(new Card(rank, suit));
             }
         }
-        // Mélange le deck entre 10 et 20 fois pour être sûr que ce soit bien mélangé
+        // Mélange le deck entre 10 et 20 fois pour être sûr que ce soit bien mélangé.
         Random rand = new Random();
         for (int i = 0; i < Math.max(10, rand.nextInt(20)); i++) {
             this.shuffleDeck();
@@ -26,12 +30,12 @@ public class Deck {
     }
 
     /**
-     * Mélange le deck de manière aléatoire en utilisant un deck temporaire
+     * Mélange le deck de manière aléatoire en utilisant un deck temporaire.
      */
     public void shuffleDeck() {
         Random rand = new Random();
         ArrayList<Card> tempDeck = new ArrayList<>();
-        // On utilise un deck temporaire dans lequel on stocke les cartes prises aléatoirement dans le deck de base pour pouvoir le mélanger
+        // On utilise un deck temporaire dans lequel on stocke les cartes prises aléatoirement dans le deck de base pour pouvoir le mélanger.
         while (!this.deck.isEmpty()) {
             tempDeck.add(this.deck.remove(rand.nextInt(this.deck.size())));
         }
@@ -39,8 +43,9 @@ public class Deck {
     }
 
     /**
-     * Pioche automatique pour créer une main (pour pouvoir faire nos tests plus rapidement)
-     * @return la première carte du deck si le deck n'est pas vide sinon null.
+     * Pioche automatique pour créer une main (pour pouvoir faire nos tests plus rapidement).
+     *
+     * @return La première carte du deck si le deck n'est pas vide sinon null.
      */
     public Card drawCard() {
         if (!this.deck.isEmpty()) {
@@ -52,8 +57,9 @@ public class Deck {
     }
 
     /**
-     * Pioche classique pour créer une main en vérifiant que la carte n'a pas déjà été tirée
-     * @return la carte c choisie si le deck contient la carte, null si le deck et vide et s'appelle elle même si la carte a déjà été piochée
+     * Pioche classique pour créer une main en vérifiant que la carte n'a pas déjà été tirée.
+     *
+     * @return La carte c choisie si le deck contient la carte, null si le deck et vide et s'appelle elle-même si la carte a déjà été piochée.
      */
     public Card takeCard(Card c) {
         if (this.deck.isEmpty()) {
@@ -61,7 +67,7 @@ public class Deck {
             System.exit(0);
             return null;
         }
-        // Si la carte est contenue dans le deck on la pioche sinon on demande de saisir une nouvelle carte
+        // Si la carte est contenue dans le deck on la pioche sinon on demande de saisir une nouvelle carte.
         if (this.deck.contains(c)) {
             for (int i = 0; i < this.deck.size(); i++) {
                 if (this.deck.get(i).equals(c)) {
@@ -75,14 +81,14 @@ public class Deck {
     }
 
     /**
-     * @return le deck
+     * @return Le deck.
      */
     public ArrayList<Card> getDeck() {
         return deck;
     }
 
     /**
-     * @return un string donnant le deck et le nombre de cartes
+     * @return Un string donnant le deck et le nombre de cartes.
      */
     @Override
     public String toString() {
